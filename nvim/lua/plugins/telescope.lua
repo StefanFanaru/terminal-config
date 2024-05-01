0f281e2e4be9a69b05f756a385a5d5c94c5428a8
Stefan Fanaru <stefan.fanaru@geminicad.com>
2024-05-01 14:49:55 +0300

    telescope fixes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -45,7 +45,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		-- [[ Configure Telescope ]]
 		-- See `:help telescope` and `:help telescope.setup()`
 		local actions = require("telescope.actions")
-		local files_options = {
+		local custom_picker_options = {
 			previewer = false,
 			theme = "dropdown",
 			layout_config = { height = 0.5, width = 0.6 },
@@ -67,8 +67,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
 				-- end,
 			},
 			pickers = {
-				git_files = files_options,
-				find_files = files_options,
+				git_files = custom_picker_options,
+				find_files = custom_picker_options,
+				live_grep = custom_picker_options,
 			},
 			-- pickers = {
 			-- 	git_files = require("stefanaru.helpers.telescopePickers").prettyFilesPicker({ picker = "git_files" }),
@@ -89,18 +90,17 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		-- vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>sp", builtin.git_files, { desc = "[S]earch [P]roject files (Git)" })
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
 		vim.keymap.set("n", "<leader>sf", function()
-			customPickers.prettyFilesPicker({ picker = "find_files" })
+			customPickers.prettyFilesPicker()
+		end)
+		vim.keymap.set("n", "<leader>sg", function()
+			customPickers.prettyGrepPicker()
 		end)
 
 		-- vim.api.nvim_set_keymap(
--- nvim/lua/stefanaru/helpers/telescopePickers.lua
+++ nvim/lua/stefanaru/helpers/telescopePickers.lua
@@ -1,5 +1,6 @@
+-- Sourced from here: https://github.com/nvim-telescope/telescope.nvim/issues/2014
 -- Declare the module
-local telescopePickers = {}
+local M = {}
 
 -- Store Utilities we'll use frequently
 local utils = require("telescope.utils")
@@ -7,34 +8,12 @@ local telescopeMakeEntryModule = require("telescope.make_entry")
 local plenaryStrings = require("plenary.strings")
 local devIcons = require("nvim-web-devicons")
 local telescopeEntryDisplayModule = require("telescope.pickers.entry_display")
-local Path = require("plenary.path")
-local conf = require("telescope.config").values
 -- Obtain Filename icon width
 -- --------------------------
 -- INSIGHT: This width applies to all icons that represent a file type
 local fileTypeIconWidth = plenaryStrings.strdisplaywidth(devIcons.get_icon("fname", { default = true }))
 
----- Helper functions ----
-
--- Gets the File Path and its Tail (the file name) as a Tuple
-function telescopePickers.getPathAndTail(fileName)
-	-- Get the Tail
-	local bufferNameTail = utils.path_tail(fileName)
-
-	-- Now remove the tail from the Full Path
-	local pathWithoutTail = require("plenary.strings").truncate(fileName, #fileName - #bufferNameTail, "")
-
-	-- Apply truncation and other pertaining modifications to the path according to Telescope path rules
-	local pathToDisplay = utils.transform_path({
-		path_display = { "truncate" },
-	}, pathWithoutTail)
-
-	-- Return as Tuple
-	return bufferNameTail, pathToDisplay
-end
-
-function telescopePickers.get_path_and_tail(filename)
-	local utils = require("telescope.utils")
+function M.get_path_and_tail(filename)
 	local bufname_tail = utils.path_tail(filename)
 	local path_without_tail = require("plenary.strings").truncate(filename, #filename - #bufname_tail, "")
 	local path_to_display = utils.transform_path({
@@ -46,54 +25,13 @@ end
 ---- Picker functions ----
 
 -- Generates a Find File picker but beautified
--- -------------------------------------------
--- This is a wrapping function used to modify the appearance of pickers that provide a Find File
--- functionality, mainly because the default one doesn't look good. It does this by changing the 'display()'
--- function that Telescope uses to display each entry in the Picker.
---
--- Adapted from: https://github.com/nvim-telescope/telescope.nvim/issues/2014#issuecomment-1541423345.
---
--- @param (table) pickerAndOptions - A table with the following format:
---                                   {
---                                      picker = '<pickerName>',
---                                      (optional) options = { ... }
---                                   }
-function telescopePickers.prettyFilesPicker(pickerAndOptions)
-	-- Parameter integrity check
-	if type(pickerAndOptions) ~= "table" or pickerAndOptions.picker == nil then
-		print(
-			"Incorrect argument format. Correct format is: { picker = 'desiredPicker', (optional) options = { ... } }"
-		)
+function M.prettyFilesPicker()
+	options = {}
 
-		-- Avoid further computation
-		return
-	end
-
-	-- Ensure 'options' integrity
-	options = pickerAndOptions.options or {}
-
-	-- Use Telescope's existing function to obtain a default 'entry_maker' function
-	-- ----------------------------------------------------------------------------
-	-- INSIGHT: Because calling this function effectively returns an 'entry_maker' function that is ready to
-	--          handle entry creation, we can later call it to obtain the final entry table, which will
-	--          ultimately be used by Telescope to display the entry by executing its 'display' key function.
-	--          This reduces our work by only having to replace the 'display' function in said table instead
-	--          of having to manipulate the rest of the data too.
-	--
-	--  Set CWD options for git_files
-	if pickerAndOptions.picker == "git_files" then
-		telescopePickers.set_opts_cwd(options)
-	end
-
-	local originalEntryMaker = telescopeMakeEntryModule.gen_from_file(options)
-
-	-- INSIGHT: 'entry_maker' is the hardcoded name of the option Telescope reads to obtain the function that
-	--          will generate each entry.
-	-- INSIGHT: The paramenter 'line' is the actual data to be displayed by the picker, however, its form is
-	--          raw (type 'any) and must be transformed into an entry table.
+	--- //////// item stylish.
+	local entry_make = telescopeMakeEntryModule.gen_from_file(options)
 	options.entry_maker = function(line)
-		-- https://github.com/nvim-telescope/telescope.nvim/issues/2014
-		local originalEntryTable = originalEntryMaker(line)
+		local entry = entry_make(line)
 		local displayer = telescopeEntryDisplayModule.create({
 			separator = " ",
 			items = {
@@ -102,138 +40,33 @@ function telescopePickers.prettyFilesPicker(pickerAndOptions)
 				{ remaining = true },
 			},
 		})
-		originalEntryTable.display = function(et)
-			local icon, iconhl = utils.get_devicons(et.value)
-			local fileName = et.value:match("([^/]+)$")
+		entry.display = function(et)
+			-- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/make_entry.lua
+			local tail_raw, path_to_display = M.get_path_and_tail(et.value)
+			local tail = tail_raw .. " "
+			local icon, iconhl = utils.get_devicons(tail_raw)
+
 			return displayer({
 				{ icon, iconhl },
-				fileName .. " ",
-				{ et.value, "TelescopeResultsComment" },
+				tail,
+				{ "test/test", "TelescopeResultsComment" },
 			})
 		end
-		return originalEntryTable
+		return entry
 	end
+	---/// end item stylish
 
-	-- Finally, check which file picker was requested and open it with its associated options
-	if pickerAndOptions.picker == "find_files" then
-		require("telescope.builtin").find_files(options)
-	elseif pickerAndOptions.picker == "git_files" then
-		options.show_untracked = true
-		require("telescope.builtin").git_files(options)
-	elseif pickerAndOptions.picker == "oldfiles" then
-		require("telescope.builtin").oldfiles(options)
-	elseif pickerAndOptions.picker == "" then
-		print("Picker was not specified")
-	else
-		print("Picker is not supported by Pretty Find Files")
-	end
+	require("telescope.builtin").find_files(options)
 end
 
-local current_path_toplevel = function()
-	local gitdir = vim.fn.finddir(".git", vim.fn.expand("%:p") .. ";")
-	if gitdir == "" then
-		return nil
-	end
-	return Path:new(gitdir):parent():absolute()
-end
+function M.prettyGrepPicker()
+	options = {}
 
-local try_worktrees = function(opts)
-	local worktrees = conf.git_worktrees
-
-	if vim.tbl_islist(worktrees) then
-		for _, wt in ipairs(worktrees) do
-			if vim.startswith(opts.cwd, wt.toplevel) then
-				opts.toplevel = wt.toplevel
-				opts.gitdir = wt.gitdir
-				if opts.use_git_root then
-					opts.cwd = wt.toplevel
-				end
-				return
-			end
-		end
-	end
-
-	error(opts.cwd .. " is not a git directory")
-end
-
--- Needed for git_files to work
-function telescopePickers.set_opts_cwd(opts)
-	opts.use_git_root = vim.F.if_nil(opts.use_git_root, true)
-	if opts.cwd then
-		opts.cwd = utils.path_expand(opts.cwd)
-	elseif opts.use_file_path then
-		opts.cwd = current_path_toplevel()
-		if not opts.cwd then
-			opts.cwd = vim.fn.expand("%:p:h")
-			try_worktrees(opts)
-			return
-		end
-	else
-		opts.cwd = vim.loop.cwd()
-	end
-
-	local toplevel, ret = utils.get_os_command_output({ "git", "rev-parse", "--show-toplevel" }, opts.cwd)
-
-	if ret ~= 0 then
-		local in_worktree = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" }, opts.cwd)
-		local in_bare = utils.get_os_command_output({ "git", "rev-parse", "--is-bare-repository" }, opts.cwd)
-
-		if in_worktree[1] ~= "true" and in_bare[1] ~= "true" then
-			try_worktrees(opts)
-		elseif in_worktree[1] ~= "true" and in_bare[1] == "true" then
-			opts.is_bare = true
-		end
-	else
-		if opts.use_git_root then
-			opts.cwd = toplevel[1]
-		end
-	end
-end
--- Generates a Grep Search picker but beautified
--- ----------------------------------------------
--- This is a wrapping function used to modify the appearance of pickers that provide Grep Search
--- functionality, mainly because the default one doesn't look good. It does this by changing the 'display()'
--- function that Telescope uses to display each entry in the Picker.
---
--- @param (table) pickerAndOptions - A table with the following format:
---                                   {
---                                      picker = '<pickerName>',
---                                      (optional) options = { ... }
---                                   }
-function telescopePickers.prettyGrepPicker(pickerAndOptions)
-	-- Parameter integrity check
-	if type(pickerAndOptions) ~= "table" or pickerAndOptions.picker == nil then
-		print(
-			"Incorrect argument format. Correct format is: { picker = 'desiredPicker', (optional) options = { ... } }"
-		)
-
-		-- Avoid further computation
-		return
-	end
-
-	-- Ensure 'options' integrity
-	options = pickerAndOptions.options or {}
-
-	-- Use Telescope's existing function to obtain a default 'entry_maker' function
-	-- ----------------------------------------------------------------------------
-	-- INSIGHT: Because calling this function effectively returns an 'entry_maker' function that is ready to
-	--          handle entry creation, we can later call it to obtain the final entry table, which will
-	--          ultimately be used by Telescope to display the entry by executing its 'display' key function.
-	--          This reduces our work by only having to replace the 'display' function in said table instead
-	--          of having to manipulate the rest of the data too.
 	local originalEntryMaker = telescopeMakeEntryModule.gen_from_vimgrep(options)
 
-	-- INSIGHT: 'entry_maker' is the hardcoded name of the option Telescope reads to obtain the function that
-	--          will generate each entry.
-	-- INSIGHT: The paramenter 'line' is the actual data to be displayed by the picker, however, its form is
-	--          raw (type 'any) and must be transformed into an entry table.
 	options.entry_maker = function(line)
-		-- Generate the Original Entry table
 		local originalEntryTable = originalEntryMaker(line)
 
-		-- INSIGHT: An "entry display" is an abstract concept that defines the "container" within which data
-		--          will be displayed inside the picker, this means that we must define options that define
-		--          its dimensions, like, for example, its width.
 		local displayer = telescopeEntryDisplayModule.create({
 			separator = " ", -- Telescope will use this separator between each entry item
 			items = {
@@ -244,56 +77,15 @@ function telescopePickers.prettyGrepPicker(pickerAndOptions)
 			},
 		})
 
-		-- LIFECYCLE: At this point the "displayer" has been created by the create() method, which has in turn
-		--            returned a function. This means that we can now call said function by using the
-		--            'displayer' variable and pass it actual entry values so that it will, in turn, output
-		--            the entry for display.
-		--
-		-- INSIGHT: We now have to replace the 'display' key in the original entry table to modify the way it
-		--          is displayed.
-		-- INSIGHT: The 'entry' is the same Original Entry Table but is is passed to the 'display()' function
-		--          later on the program execution, most likely when the actual display is made, which could
-		--          be deferred to allow lazy loading.
-		--
-		-- HELP: Read the 'make_entry.lua' file for more info on how all of this works
 		originalEntryTable.display = function(entry)
-			---- Get File columns data ----
-			-------------------------------
+			local tail, pathToDisplay = M.get_path_and_tail(entry.filename)
 
-			-- Get the Tail and the Path to display
-			local tail, pathToDisplay = telescopePickers.getPathAndTail(entry.filename)
-
-			-- Get the Icon with its corresponding Highlight information
 			local icon, iconHighlight = utils.get_devicons(tail)
 
-			---- Format Text for display ----
-			---------------------------------
-
-			-- Add coordinates if required by 'options'
-			local coordinates = ""
-
-			if not options.disable_coordinates then
-				if entry.lnum then
-					if entry.col then
-						coordinates = string.format(" -> %s:%s", entry.lnum, entry.col)
-					else
-						coordinates = string.format(" -> %s", entry.lnum)
-					end
-				end
-			end
-
-			-- Append coordinates to tail
-			tail = tail .. coordinates
-
-			-- Add an extra space to the tail so that it looks nicely separated from the path
 			local tailForDisplay = tail .. " "
 
-			-- Encode text if necessary
 			local text = options.file_encoding and vim.iconv(entry.text, options.file_encoding, "utf8") or entry.text
 
-			-- INSIGHT: This return value should be a tuple of 2, where the first value is the actual value
-			--          and the second one is the highlight information, this will be done by the displayer
-			--          internally and return in the correct format.
 			return displayer({
 				{ icon, iconHighlight },
 				tailForDisplay,
@@ -305,17 +97,7 @@ function telescopePickers.prettyGrepPicker(pickerAndOptions)
 		return originalEntryTable
 	end
 
-	-- Finally, check which file picker was requested and open it with its associated options
-	if pickerAndOptions.picker == "live_grep" then
-		require("telescope.builtin").live_grep(options)
-	elseif pickerAndOptions.picker == "grep_string" then
-		require("telescope.builtin").grep_string(options)
-	elseif pickerAndOptions.picker == "" then
-		print("Picker was not specified")
-	else
-		print("Picker is not supported by Pretty Grep Picker")
-	end
+	require("telescope.builtin").live_grep(options)
 end
 
--- Return the module for use
-return telescopePickers
+return M
